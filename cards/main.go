359d3901def12abe55418ff7873104cc1df9e5fe
git-user <gwesterf@amazon.com>
2021-12-23 10:05:38 -0500

    initial

---
+++ cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initialization
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+
+	// reassignment
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
